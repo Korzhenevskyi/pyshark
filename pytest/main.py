ba5d213cbbad0b845511f3c3421f338f08296a62
Oleksandr Korzhenevskyi <aleksander3103@yandex.ua>
2021-07-22 18:32:33 +0200

    reorder files

--- pytest/main.py
+++ pytest/main.py
@@ -5,19 +5,25 @@ import json
 import requests
 import os
 
+directory = 'data'
+
 class CustomEncoder(json.JSONEncoder):
     def default(self, o):
         return o.__dict__
 
 def store_packet_as_json(packet, i):
-    with open('packet %d.json' % i, 'a') as w:
+    with open('%s/packet %d.json' % (directory, i), 'a') as w:
         w.write(json.dumps(packet, cls=CustomEncoder))
 
 def check_the_packet_for_harm(packet, url):
     requests.post(url=url, json=json.dumps(packet, cls=CustomEncoder))
 
+if not os.path.exists(directory):
+    os.mkdir(directory)
+
 capture = pyshark.LiveCapture(interface='wifi')
-capture.sniff(timeout=10)
+capture.sniff(timeout=20)
 
 for i in range(len(capture)):
+    print("packet "+str(i))
     store_packet_as_json(capture[i], i)
